43154baac1bdf9b81679b25b0c46def696d5d340
Sebas <sebasdeldi123@gmail.com>
2020-04-29 21:34:24 -0500

    Refactor project structure

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthFormComponent } from './auth-form/auth-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { TextInputComponent } from './text-input/text-input.component';
+import { AuthFormComponent } from './screens/auth/components/auth-form/auth-form.component';
+import { LoginFormComponent } from './screens/auth/screens/login/components/login-form/login-form.component';
+import { SignupFormComponent } from './screens/auth/screens/signup/components/signup-form/signup-form.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   declarations: [
