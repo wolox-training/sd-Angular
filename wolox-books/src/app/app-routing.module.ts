5de14005183cd8ddefaf1cf24c5c3183a1b52eb3
Sebas <sebasdeldi123@gmail.com>
2020-04-30 21:46:54 -0500

    Add routing for signup and login

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './screens/auth/screens/login/components/login-form/login-form.component';
+import { SignupFormComponent } from './screens/auth/screens/signup/components/signup-form/signup-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: 'signup', component: SignupFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- wolox-books/src/app/screens/auth/screens/login/components/login-form/login-form.component.ts
+++ wolox-books/src/app/screens/auth/screens/login/components/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 const SECONDARY_BUTTON_TEXT = 'Sign Up';
+const SECONDARY_BUTTON_REDIRECTION_URL = '/signup';
 
 @Component({
   selector: 'app-login-form',
@@ -12,6 +13,9 @@ export class LoginFormComponent implements OnInit {
 
   // Button text
   secondaryButtonText: string = SECONDARY_BUTTON_TEXT;
+
+  // Secondary button redirection url
+  secondaryButtonRedirectionUrl: string = SECONDARY_BUTTON_REDIRECTION_URL;
 
   constructor() { }
 
