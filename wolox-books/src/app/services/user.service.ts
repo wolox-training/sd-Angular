49869b01704f0570f82f233f97913cbcfc2545d0
Sebas <sebasdeldi123@gmail.com>
2020-05-04 11:35:24 -0500

    Add api to env vars

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { User } from '../models/user';
 export class UserService {
   constructor(private httpClient: HttpClient) {}
 
-  readonly SIGN_UP_URL = 'https://books-training-rails.herokuapp.com/api/v1/users';
+  readonly SIGN_UP_URL = environment.apiBaseUrl + '/users';
 
   createUser(newUser: User){
     return this.httpClient.post<any>(this.SIGN_UP_URL, newUser.toJson());
