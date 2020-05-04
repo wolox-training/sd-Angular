d43de53ae51d4800a91bbb78741edab89144dc12
Sebas <sebasdeldi123@gmail.com>
2020-05-04 13:29:38 -0500

    Add login integration

--- wolox-books/src/app/screens/auth/screens/login/components/login-form/login-form.component.ts
+++ wolox-books/src/app/screens/auth/screens/login/components/login-form/login-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
 
 const SECONDARY_BUTTON_TEXT = 'Sign Up';
 const SECONDARY_BUTTON_REDIRECTION_URL = '/signup';
@@ -17,8 +21,26 @@ export class LoginFormComponent implements OnInit {
   // Secondary button redirection url
   secondaryButtonRedirectionUrl: string = SECONDARY_BUTTON_REDIRECTION_URL;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
+
+  loginForm: FormGroup;
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      }
+    );
+  }
+
+  onSubmit(loginData:any) {
+    this.userService.authenticateUser(loginData.email, loginData.password).subscribe(
+      res => {
+        console.log('Headers', res.headers);
+        console.log('Body', res.body.data);
+      },
+      error => console.log('Error!', error.error.errors)
+    );
   }
 }
